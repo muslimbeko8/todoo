9fea4937f907d00011a08d9d471c57953483af65
muslimbeko8 <blakmaxmudov@gmail.com>
2024-12-06 23:20:41 +0500

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer className="py-5 border-t text-center">
+      Powered by{" "}
+      <a
+        className="underline"
+        href="https://github.com/muslimbeko8"
+        target="_blank"
+      >
+        Muslimbek
+      </a>
+    </footer>
+  );
+}
